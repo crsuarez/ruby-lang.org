28a0e061f86835618f70e7512c940266cf1b8fa5
Jean-Denis Vauguet <jd@vauguet.fr>
2011-08-07 18:36:33 +0200

    config.rb update for github pages

--- source/config.rb
+++ source/config.rb
@@ -1,9 +1,9 @@
 # Compass configuration
 #
-http_path       = "/"
+http_path       = "/ruby-lang.org/"
 css_dir         = "stylesheets"
 sass_dir        = "sass"
 images_dir      = "images"
 javascripts_dir = "javascripts"
-output_style    = :expanded # :compact or :compressed in prod
+output_style    = :compact # :compact or :compressed in prod
 # line_comments = false
