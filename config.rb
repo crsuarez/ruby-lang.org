483cdef467a06c1fa5d08758d9420e120db58e2e
Jean-Denis Vauguet <jd@vauguet.fr>
2011-08-07 18:43:54 +0200

    compass config update

--- config.rb
+++ config.rb
@@ -1,9 +1,11 @@
 # Compass configuration
 #
+relative_assets = false
 http_path       = "/ruby-lang.org/"
 css_dir         = "stylesheets"
 sass_dir        = "sass"
 images_dir      = "images"
 javascripts_dir = "javascripts"
-output_style    = :compact # :compact or :compressed in prod
+output_style    = :compressed # :compact or :compressed in prod
+
 # line_comments = false
