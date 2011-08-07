59ab01098d79ace3d76c03d2803b3cb78f20c989
Jean-Denis Vauguet <jd@vauguet.fr>
2011-08-07 20:07:05 +0200

    content update

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Compass configuration
+#
+relative_assets  = false
+http_path        = "/ruby-lang.org/"
+http_images_path = "/ruby-lang.org/images"
+http_fonts_path  = "/ruby-lang.org/fonts"
+css_dir          = "stylesheets"
+sass_dir         = "sass"
+images_dir       = "images"
+javascripts_dir  = "javascripts"
+output_style     = :compressed # :compact or :compressed in prod
+line_comments = false
+#asset_host do |asset|
+  #"http://rubylang.info/ruby-lang.org/"
+#end
